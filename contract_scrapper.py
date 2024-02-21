191f6d392908ba4e22494b4a692c30cc26f4688b
CadenParry <cadenparry02@gmail.com>
2024-02-21 17:28:48 -0500

    player search, updated output, NO TEXT FILE YET

--- contract_scrapper.py
+++ contract_scrapper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from datetime import datetime
+import re
 
 def parse_player_name(player):
     player = player.replace(' ', '-')
@@ -29,41 +30,42 @@ def get_ulr_spotrac_player(player, teams):
 def create_team_dict():
     dict = {}
     #NL West
-    dict['Arizona Diamondbacks'] = 'arizona-diamondbacks'
-    dict['Colorado Rockies'] = 'colorado-rockies'
-    dict['Los Angeles Dodgers'] = 'los-angeles-dodgers'
-    dict['San Diego Padres'] = 'san-diego-padres'
-    dict['San Francisco Giants'] = 'san-francisco-giants'
+    dict['AD'] = 'arizona-diamondbacks'
+    dict['CR'] = 'colorado-rockies'
+    dict['LAD'] = 'los-angeles-dodgers'
+    dict['SD'] = 'san-diego-padres'
+    dict['SF'] = 'san-francisco-giants'
     #NL Central
-    dict['Chicago Cubs'] = 'chicago-cubs'
-    dict['Cincinnati Reds'] = 'cincinnati-reds'
-    dict['Milwaukee Brewers'] = 'milwaukee-brewers'
-    dict['Pittsburgh Pirates'] = 'pittsburgh-pirates'
-    dict['St. Louis Cardinals'] = 'st-louis-cardinals'
+    dict['CHC'] = 'chicago-cubs'
+    dict['CIN'] = 'cincinnati-reds'
+    dict['MIL'] = 'milwaukee-brewers'
+    dict['PIT'] = 'pittsburgh-pirates'
+    dict['STL'] = 'st-louis-cardinals'
     #NL East
-    dict['Atlanta Braves'] = 'atlanta-braves'
-    dict['Miami Marlins'] = 'miami-marlins'
-    dict['New York Mets'] = 'new-york-mets'
-    dict['Philadelphia Phillies'] = 'philadelphia-phillies'
-    dict['Washington Nationals'] = 'washington-nationals'
+    dict['ATL'] = 'atlanta-braves'
+    dict['MIA'] = 'miami-marlins'
+    dict['NYM'] = 'new-york-mets'
+    dict['PHI'] = 'philadelphia-phillies'
+    dict['WSH'] = 'washington-nationals'
     #AL West
-    dict['Houston Astros'] = 'houston-astros'
-    dict['Los Angeles Angels'] = 'los-angeles-angels'
-    dict['Oakland Athletics'] = 'oakland-athletics'
-    dict['Seattle Mariners'] = 'seattle-mariners'
-    dict['Texas Rangers'] = 'texas-rangers'
+    dict['HOU'] = 'houston-astros'
+    dict['LAA'] = 'los-angeles-angels'
+    dict['OAK'] = 'oakland-athletics'
+    dict['SEA'] = 'seattle-mariners'
+    dict['TEX'] = 'texas-rangers'
     #AL Central
-    dict['Chicago White Sox'] = 'chicago-white-sox'
-    dict['Cleveland Indians'] = 'cleveland-indians'
-    dict['Detroit Tigers'] = 'detroit-tigers'
-    dict['Kansas City Royals'] = 'kansas-city-royals'
-    dict['Minnesota Twins'] = 'minnesota-twins'
+    dict['CWS'] = 'chicago-white-sox'
+    dict['CLE'] = 'cleveland-indians'
+    dict['DET'] = 'detroit-tigers'
+    dict['KC'] = 'kansas-city-royals'
+    dict['MIN'] = 'minnesota-twins'
     #AL East
-    dict['Baltimore Orioles'] = 'baltimore-orioles'
-    dict['Boston Red Sox'] = 'boston-red-sox'
-    dict['New York Yankees'] = 'new-york-yankees'
-    dict['Tampa Bay Rays'] = 'tampa-bay-rays'
-    dict['Toronto Blue Jays'] = 'toronto-blue-jays'
+    dict['BAL'] = 'baltimore-orioles'
+    dict['BOS'] = 'boston-red-sox'
+    dict['NYY'] = 'new-york-yankees'
+    dict['TB'] = 'tampa-bay-rays'
+    dict['TOR'] = 'toronto-blue-jays'
+   
     return dict
 
 def make_soup(response):
@@ -77,24 +79,132 @@ def get_player_info(soup):
         year = datetime.strptime(year, '%b %d %Y').year
         info = transaction.find('span', class_='transdesc').text.strip()
 
-        # and (year not in player_info) 
-        # -> this is to avoid overwriting the same year but is not curretnly used
         if ('$' in info):
-            data = player_info.get(year)
-
-            if data is None:
-                player_info[year] = info
+            if year not in player_info:
+                player_info[year] = [info]
             else:
-                player_info[year] = player_info[year] + '\n' + info
+                if info not in player_info[year]:
+                    player_info[year].append(info)
                 
     return player_info
+#v1
+# def get_largest_contract(player_info):
+#     largest_contract = 0
+#     for year in player_info:
+#         for contract in player_info[year]:
+#             match = re.search(r'\$(\d+(?:\.\d+)?)( million)?', contract)
+#             if match:
+#                 value = float(match.group(1))
+#                 if match.group(2):
+#                     value *= 1e6  # convert to millions if 'million' is present
+#                 if value > largest_contract:
+#                     largest_contract = value
+#     return largest_contract
+
+#v2
+# def get_largest_contract(player_info):
+#     largest_contract = 0
+#     for year in player_info:
+#         for contract in player_info[year]:
+#             contract = contract.replace('$', '')
+#             contract = contract.replace(',', '')
+#             contract = contract.split(' ')
+#             if len(contract) > 1:
+#                 if contract[1] == 'million':
+#                     if float(contract[0]) > largest_contract:
+#                         largest_contract = float(contract[0])
+#     return largest_contract
+
+#v3
+# def get_largest_contract(player_info):
+#     largest_contract = 0
+#     largest_contract_year = None
+#     for year in player_info:
+#         for contract in player_info[year]:
+#             match = re.search(r'\$(\d+(?:\.\d+)?)( million)?', contract)
+#             if match:
+#                 value = float(match.group(1))
+#                 if match.group(2):
+#                     value *= 1e6  # convert to millions if 'million' is present
+#                 if value > largest_contract:
+#                     largest_contract = value
+#                     largest_contract_year = year
+#     return largest_contract_year, "${:,.2f}".format(largest_contract)
+
+#v4
+# def get_largest_contract(player_info):
+#     largest_contract = 0
+#     largest_contract_year = None
+#     largest_contract_duration = None
+#     for year in player_info:
+#         for contract in player_info[year]:
+#             match = re.search(r'\$(\d+(?:\.\d+)?)( million)?', contract)
+#             duration_match = re.search(r'(\d+) year', contract)
+#             if match:
+#                 value = float(match.group(1))
+#                 if match.group(2):
+#                     value *= 1e6  # convert to millions if 'million' is present
+#                 if value > largest_contract:
+#                     largest_contract = value
+#                     largest_contract_year = year
+#                     if duration_match:
+#                         largest_contract_duration = int(duration_match.group(1))
+#     return largest_contract_year, "${:,.2f}".format(largest_contract), largest_contract_duration
+
+#v5
+# def get_largest_contract(player_info):
+#     largest_contract = 0
+#     largest_contract_year = None
+#     largest_contract_duration = None
+#     largest_contract_team = None
+#     for year in player_info:
+#         for contract in player_info[year]:
+#             match = re.search(r'\$(\d+(?:\.\d+)?)( million)?', contract)
+#             duration_match = re.search(r'(\d+) year', contract)
+#             team_match = re.search(r'with (.+?) \(', contract)
+#             if match:
+#                 value = float(match.group(1))
+#                 if match.group(2):
+#                     value *= 1e6  # convert to millions if 'million' is present
+#                 if value > largest_contract:
+#                     largest_contract = value
+#                     largest_contract_year = year
+#                     if duration_match:
+#                         largest_contract_duration = int(duration_match.group(1))
+#                     if team_match:
+#                         largest_contract_team = team_match.group(1)
+#     return largest_contract_year, "${:,.2f}".format(largest_contract), largest_contract_duration, largest_contract_team
+
+#v6
+def get_largest_contract(player_info):
+    largest_contract = 0
+    largest_contract_year = None
+    largest_contract_duration = None
+    largest_contract_team = None
+    for year in player_info:
+        for contract in player_info[year]:
+            match = re.search(r'\$(\d+(?:\.\d+)?)( million)?', contract)
+            duration_match = re.search(r'(\d+) year', contract)
+            team_match = re.search(r'\((.*?)\)', contract)  # get the text within parentheses
+            if match:
+                value = float(match.group(1))
+                if match.group(2):
+                    value *= 1e6  # convert to millions if 'million' is present
+                if value > largest_contract:
+                    largest_contract = value
+                    largest_contract_year = year
+                    if duration_match:
+                        largest_contract_duration = int(duration_match.group(1))
+                    if team_match:
+                        largest_contract_team = team_match.group(1)
+    return largest_contract_year, "${:,.2f}".format(largest_contract), largest_contract_duration, largest_contract_team
 
 def main():
     while True:
-        user_chosen_player = parse_player_name(input('Enter a player (X to exit): '))
-        # if user_chosen_player == 'X' or 'x':
-        #     break
+        player_name = input('Enter a player (X to exit): ')
 
+        user_chosen_player = parse_player_name(player_name)
+    
         team_dict = create_team_dict()
         
         player_ulr = get_ulr_spotrac_player(str(user_chosen_player), team_dict)
@@ -102,8 +212,15 @@ def main():
         response = request_base_html(player_ulr)
 
         player_info = get_player_info(make_soup(response))
+        
+        largest_contract = get_largest_contract(player_info)
 
-        for key in player_info:
-            print(key, player_info[key])
-   
+        print('player: ' + player_name)
+        print('year: ' + str(largest_contract[0]))
+        print('contract value: ' + str(largest_contract[1]))
+        print('duration: ' + str(largest_contract[2]))
+        print('team: ' + str(largest_contract[3]))
+        print('\n')
+
+        
 main()
